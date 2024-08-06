0ba9b8c25c374d9426c9b1fa56dc65f2dc3c623a
OtavioMendesLima <otaviovinteum21@gmail.com>
2024-08-06 20:37:07 -0300

    Introduzindo mondoDB.

---
+++ create-database.mongodb.js
@@ -0,0 +1,11 @@
+/*Nome do Banco de Dados: */
+const database = 'BD3-OTAVIO-AULA';
+
+/* Nome da coleção de Banco de Dados: */
+const collection = 'Livraria';
+
+/* Cria ou acessa o Banco de Dados: */
+use (database)
+
+/* Criar uma coleçÃO de Dados: */
+db.createCollection (collection);
