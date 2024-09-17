7d90010d3e1213a347a5814e0c6eea698f341620
OtavioMendesLima <otaviovinteum21@gmail.com>
2024-09-17 19:50:20 -0300

    Introduzindo operadores não relacionais ao banco.

--- Pw3/src/components/pages/Home.jsx
+++ Pw3/src/components/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import style from './Home.module.css'
+import Style from './Home.module.css'
 
 const Home = () => {
-    return(
-        <section>
-            <h1>Bem Vindo ao <span>LIBRI</span></h1>
-            <p>Sua platforma web de gestão de livros.</p>
-            <img src="./book_home.jpg"></img>
-        </section>
+    return (
+        <div className={Style.home_container}>
+            <section>
+                <h1>Bem Vindo ao <span>LIBRI</span></h1>
+                <p>Sua plataforma web de gestão de livros.</p>
+                <img src="./book_home.jpg"></img>
+            </section>
+        </div>
     )
 }
 
