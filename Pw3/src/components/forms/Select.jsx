903adac443832ff6130e6ec4a17b63de242bd86a
OtavioMendesLima <otaviovinteum21@gmail.com>
2024-10-03 19:29:19 -0300

    Conenctando Back e Front

--- Pw3/src/components/forms/Select.jsx
+++ Pw3/src/components/forms/Select.jsx
@@ -1,16 +1,21 @@
 import Style from './Select.module.css'
 
-function Select({ name, text }) {
+function Select({ name, text, options }) {
     return (
 
         <div className={Style.form_control}>
-            <label htmlFor={name}>{text}</label>
+            <label htmlFor={name}>{name}</label>
             <select name={name} id={name}>
-                <option>Selecione uma categoria:</option>
-                <option>Ficção Cintífica</option>
-                <option>Fantasia Heroica</option>
-                <option>Suspense</option>
-                <option>Terror </option>
+            <option>Selecione uma categoria:</option>
+
+                {
+                    options.map((option)=>{
+                        // console.log(option.cod_categoria + ' - ' + option.nome_categoria)
+                    return <option>{option.nome_categoria}</option>
+                    })
+                }
+
+                
             </select>
         </div>
     )
--- Pw3/src/components/pages/CreateBooks.jsx
+++ Pw3/src/components/pages/CreateBooks.jsx
@@ -1,14 +1,44 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import style from './CreateBooks.module.css'
 import Input from "../forms/Input"
 import Select from "../forms/Select"
 import Button from "../forms/Button"
 
 const CreateBooks = () => {
+
+const [categorias, setCategorias] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/listagemCateorias', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Acess-Control-Allow-Origin': '*',
+        'Acess-Control-Allow-Headers': '*',
+
+      }
+    }).then(
+      (resp) => 
+        // console.log('Running' + resp)
+        resp.json()
+      
+    ).then(
+      (data) =>{
+        console.log('DATA: ' + data.data[3].nome_categoria)
+        setCategorias(data.data)
+      }
+    ).catch(
+      (error)=>{
+        console.log(error)
+      }
+    )
+  },[]);
+
   return (
     <section className={style.create_book_container}>
       <h1>CADASTRO DE LIVROS</h1>
-      
+
       <Input
         type='text'
         name='txt_livro'
@@ -27,14 +57,15 @@ const CreateBooks = () => {
         text='Descrição do Livro'
         placeHolder='Descricao do livro' />
 
-        <Select
-            name='Categoria'
-            text='Escolha uma categoria'
-        />
+      <Select
+        name='Categoria'
+        text='Escolha uma categoria'
+        options={categorias}
+      />
 
-        <Button
-          rotulo="Cadastrar Livro"
-        />
+      <Button
+        rotulo="Cadastrar Livro"
+      />
 
     </section>
   )
